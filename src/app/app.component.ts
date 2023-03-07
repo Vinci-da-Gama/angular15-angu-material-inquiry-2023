cbb812e624e380c56202f02d4d5273581db3acc4
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-07 13:41:53 +1100

    a15m: change prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'angular15app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angu15-angumaterial-inquiry';
