f7665063980474f27d8a1e09d5bc79f0acfbe669
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-07 14:35:18 +1100

    a15m: add lint

---
+++ src/app/store/index.ts
@@ -0,0 +1,9 @@
+import { ActionReducerMap } from '@ngrx/store';
+
+export interface AppState {
+  // Define your app state here
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+  // Define your reducers here
+};
